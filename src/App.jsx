74a55b48c8a9775241d1c646148bf96ad0dbc8f5
Mugisha Kevine <Kevinemug>
2023-10-02 15:30:21 +0200

    fetching random urls

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import {Routes,Route,Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-
+import "./server"
+import Vans from './pages/Vans';
+// import { HashRouter } from "react-router-dom";
 function App() {
 
   return (
@@ -11,12 +13,15 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
-    <Routes>
+      <Routes>
       <Route path="/" element ={<Home/>}/>
       <Route  path="/about" element ={<About/>}/>
+      <Route  path="/vans" element ={<Vans/>}/>
     </Routes>
+
     </>
   )
 }
